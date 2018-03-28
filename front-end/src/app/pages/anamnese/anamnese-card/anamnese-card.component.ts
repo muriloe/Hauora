99f959e127906c6814389625857f7121471d7b46
Murilo Erhardt <murilo0121@gmail.com>
2018-03-28 01:52:21 -0300

    Card componente da anamnese

---
+++ front-end/src/app/pages/anamnese/anamnese-card/anamnese-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-anamnese-card',
+  templateUrl: './anamnese-card.component.html',
+  styleUrls: ['./anamnese-card.component.scss'],
+})
+export class AnamneseCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- front-end/src/app/pages/anamnese/anamnese.module.ts
+++ front-end/src/app/pages/anamnese/anamnese.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ThemeModule } from '../../@theme/theme.module';
 
 import { AnamneseComponent } from './anamnese.component';
 import { routing } from './anamnese.routing';
+import { AnamneseCardComponent } from './anamnese-card/anamnese-card.component';
 
 @NgModule({
   imports: [
+    ThemeModule,
     CommonModule,
     FormsModule,
     routing,
   ],
   declarations: [
     AnamneseComponent,
+    AnamneseCardComponent,
   ],
 })
 export class AnamneseModule {}
