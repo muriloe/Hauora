e3c5d5527e5a955cee40ec4787af7d86349a5a22
Murilo Erhardt <murilo0121@gmail.com>
2018-03-28 00:53:25 -0300

    Inicio da criação da página de anamnese

--- front-end/src/app/pages/anamnese/anamnese.component.ts
+++ front-end/src/app/pages/anamnese/anamnese.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-anamnese',
-  template: `<strong>My page content here</strong>`,
+  templateUrl: './anamnese.component.html',
 })
 export class AnamneseComponent {
   constructor() {}
