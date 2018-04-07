593f57317feaecbc00c82911dd014b33a9116d1a
Murilo Erhardt <murilo0121@gmail.com>
2018-04-07 04:58:09 -0300

    novo endereço banco de dados

--- back-end/config/index.js
+++ back-end/config/index.js
@@ -3,7 +3,7 @@ var configValues = require('./config');
 module.exports = {
 
     getDbConnectionString: function() {
-        return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds237389.mlab.com:37389/hauoradb';
+        return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds237379.mlab.com:37379/hauoradb';
     }
 
 }
