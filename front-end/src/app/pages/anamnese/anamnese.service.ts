ab4c59a0ad8a7e5601b2f3c3a27b155fe928f262
Murilo Erhardt <murilo0121@gmail.com>
2018-04-08 18:21:14 -0300

    Criação da controller de cliente back-end 3

--- front-end/src/app/pages/anamnese/anamnese.service.ts
+++ front-end/src/app/pages/anamnese/anamnese.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-//import { Subject } from 'rxjs/Subject';
-//import { Cliente } from '../../shared/model/cliente.model';
+// import { Subject } from 'rxjs/Subject';
+// import { Cliente } from '../../shared/model/cliente.model';
 
 @Injectable()
 export class RecipeService {
-    //private clientesAnamnese: Cliente[];
+    // private clientesAnamnese: Cliente[];
 
 }
