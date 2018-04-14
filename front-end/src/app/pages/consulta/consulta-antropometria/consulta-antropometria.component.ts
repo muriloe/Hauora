e985d70d4c03b39bd88d589ae49b5627aae7d7cd
Murilo Erhardt <murilo0121@gmail.com>
2018-04-14 03:31:24 -0300

    iniciado a inclusão de formulários na tela de consulta

---
+++ front-end/src/app/pages/consulta/consulta-antropometria/consulta-antropometria.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'ngx-consulta-antropometria',
+    templateUrl: './consulta-antropometria.component.html',
+    styleUrls: ['./consulta-antropometria.component.scss'],
+  })
+  export class ConsultaAntropometriaComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+
+}
--- front-end/src/app/pages/consulta/consulta.module.ts
+++ front-end/src/app/pages/consulta/consulta.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ThemeModule } from '../../@theme/theme.module';
 
 import { ConsultaComponent } from './consulta.component';
+import { ConsultaAntropometriaComponent } from './consulta-antropometria/consulta-antropometria.component';
 import { routing } from './consulta.routing';
 
 @NgModule({
   imports: [
+    ThemeModule,
     CommonModule,
     FormsModule,
     routing,
   ],
   declarations: [
     ConsultaComponent,
+    ConsultaAntropometriaComponent,
   ],
 })
 export class ConsultaModule {}
