771ded184954ca3a6e0d080318427f9091191d0c
Murilo Erhardt <murilo0121@gmail.com>
2018-05-01 20:32:36 -0300

    Finalizado exibir alimentos consumidos

--- front-end/src/app/pages/consulta/consulta-antropometria/consulta-anamnese-modal/consumo-card/consumo-card.ts
+++ front-end/src/app/pages/consulta/consulta-antropometria/consulta-anamnese-modal/consumo-card/consumo-card.ts
@@ -5,46 +5,57 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
     selector: 'ngx-consumo-card',
     template: `
       <div>
-        <nb-user [picture]="tipoIcon" [name]="tipo" [title]="sentimento" size="large"></nb-user>
+        <nb-card>
+          <nb-card-body>
+            <nb-user [picture]="tipoIcon" [name]="tipo" [title]="sentimento" size="large"></nb-user>
+            <br>
+            <h6 *ngIf="descricao">Descrição</h6>
+            <p *ngIf="descricao">{{descricao}}</p>
+            <h6 *ngIf="comentario">Comentário</h6>
+            <p *ngIf="comentario">{{comentario}}</p>
+            <div class="detail font-w-light">{{data | date: 'dd/MM/yyyy hh:mm'}} </div>
+          </nb-card-body>
+        </nb-card>
       </div>
     `,
 })
 
 export class ConsumoCardComponent implements OnInit {
-  ngOnInit(): void {
-    this.sentimento = 'Sentindo-se ' + this.sentimento;
-    if (this.tipo === 'CAFE_DA_MANHA') {
-      // tslint:disable-next-line:quotemark
-      this.tipoIcon = "assets/images/mealIcons/breakfastIcon.png";
-      this.tipo = 'Café da manhã';
-    }
-    if (this.tipo === 'LANCHE_DA_MANHA') {
-       // tslint:disable-next-line:quotemark
-      this.tipoIcon = "assets/images/mealIcons/snackIcon.png";
-      this.tipo = 'Lanche';
-    }
-    if (this.tipo === 'ALMOCO') {
-       // tslint:disable-next-line:quotemark
-      this.tipoIcon = "assets/images/mealIcons/lunchIcon.png";
-      this.tipo = 'Almoço';
-    }
-    if (this.tipo === 'JANTA') {
-       // tslint:disable-next-line:quotemark
-      this.tipoIcon = "assets/images/mealIcons/dinnerIcon.png";
-      this.tipo = 'Janta';
-    }
-    if (this.tipo === 'LANCHE') {
-       // tslint:disable-next-line:quotemark
-      this.tipoIcon = "assets/images/mealIcons/snackIcon.png";
-      this.tipo = 'Lanche';
-    }
-  }
 
     @Input() icon: string;
     @Input() tipo: string;
     @Input() sentimento: string;
-    @Input() descrição: string;
+    @Input() descricao: string;
     @Input() comentario: string;
     @Input() data: Date;
     tipoIcon: string;
+
+    ngOnInit(): void {
+      this.sentimento = 'Sentindo-se ' + this.sentimento;
+      if (this.tipo === 'CAFE_DA_MANHA') {
+        // tslint:disable-next-line:quotemark
+        this.tipoIcon = "assets/images/mealIcons/breakfastIcon.png";
+        this.tipo = 'Café da manhã';
+      }
+      if (this.tipo === 'LANCHE_DA_MANHA') {
+         // tslint:disable-next-line:quotemark
+        this.tipoIcon = "assets/images/mealIcons/snackIcon.png";
+        this.tipo = 'Lanche';
+      }
+      if (this.tipo === 'ALMOCO') {
+         // tslint:disable-next-line:quotemark
+        this.tipoIcon = "assets/images/mealIcons/lunchIcon.png";
+        this.tipo = 'Almoço';
+      }
+      if (this.tipo === 'JANTA') {
+         // tslint:disable-next-line:quotemark
+        this.tipoIcon = "assets/images/mealIcons/dinnerIcon.png";
+        this.tipo = 'Janta';
+      }
+      if (this.tipo === 'LANCHE') {
+         // tslint:disable-next-line:quotemark
+        this.tipoIcon = "assets/images/mealIcons/snackIcon.png";
+        this.tipo = 'Lanche';
+      }
+    }
 }
