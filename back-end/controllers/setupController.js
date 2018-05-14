282bd0fced439be141ecce0c841b07eba6d29cb0
Murilo Erhardt <murilo0121@gmail.com>
2018-05-14 13:15:19 +0000

    Finalizado grupos a alimentos

--- back-end/controllers/setupController.js
+++ back-end/controllers/setupController.js
@@ -25,9 +25,14 @@ module.exports = function(app){
     app.get('/api/iniciaAlimentos', function(req, res){
      
         //criação dos grupos
-        var carboidratos = new Grupo({titulo: 'Carboidratos'});
-        var carnesEOvos = new Grupo({titulo: 'Carnes e ovos'});
-        var frutas = new Grupo({titulo: 'Frutas'});  
+        var carboidratos = new Grupo({titulo: 'Carboidratos Média (150 Kcal)'});
+        var carnesEOvos = new Grupo({titulo: 'Carnes e ovos Média (190 Kcal)'});
+        var frutas = new Grupo({titulo: 'Frutas Média (70 Kcal)'});  
+        var legumesVerduras = new Grupo({titulo: 'Legumes e Verduras Média (15 Kcal)'});  
+        var leguminosas = new Grupo({titulo: 'Leguminosas Média (55 Kcal)'});  
+        var leiteQueijoIogurte = new Grupo({titulo: 'Leites, Queijos e Iogurtes Média (120 Kcal)'});
+        var oleosGorduras = new Grupo({titulo: 'Óleos e Gorduras Média (73 Kcal)'});
+        
 
         //criacao dos alimentos
         var alimentos = [   {nome: 'Arroz Branco Cozido', porcao: '4 Col. Sopa (125g/ml)', grupo: carboidratos._id},
@@ -111,7 +116,101 @@ module.exports = function(app){
                             {nome: 'Uva Comum  ', porcao: '22 Unidades (100g/ml)', grupo: frutas._id},
                             {nome: 'Uva Itália  ', porcao: '8 Uvas (100g/ml)', grupo: frutas._id},
                             {nome: 'Uva Passa ', porcao: '1 Colher de Sopa (17g/ml)', grupo: frutas._id},
-                            {nome: 'Uva Rubi  ', porcao: '8 Uvas (100g/ml)', grupo: frutas._id},];
+                            {nome: 'Uva Rubi  ', porcao: '8 Uvas (100g/ml)', grupo: frutas._id},
+                            
+                            
+                            {nome: 'Abóbora Cozida', porcao: '1 Colher de Sopa e Meia (53,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Abobrinha Cozida', porcao: '3 Colheres de Sopa (81,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Acelga Cozida ', porcao: '2 Colheres de Sopa e Meia (81,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Acelga Crua Picada', porcao: '9 Colheres de Sopa (90,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Agrião', porcao: '22 Ramos (130,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Aipo Cru', porcao: '2 Unidades (80,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Alcachofra Cozida ', porcao: '1/4 de Unidade (35,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Alface', porcao: '15 Folhas (130,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Almeirão', porcao: '5 Folhas (65,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Aspargo em Conserva ', porcao: '8 Unidades (80,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Berinjela Cozida', porcao: '2 Colheres de Sopa (60,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Beterraba Cozida', porcao: '3 Fatias (30,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Beterraba Crua Ralada ', porcao: '2 Colheres de Sopa (40,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Brócolis Cozido ', porcao: '4 Colheres de Sopa (60,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Broto de Alfafa Cru ', porcao: '1 Xícara de Chá e Meia (50,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Broto de Feijão Cozido', porcao: '1 Colher de Servir e Meia (81,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Cenoura Cozida', porcao: '7 Fatias (35,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Cenoura Cozida Picada ', porcao: '1 Colher de Servir (36,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Cenoura Crua Picada ', porcao: '1 Colher de Servir (36,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Chuchu Cozido ', porcao: '2 Colheres de Servir e Meia (57,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Couve-Flor Cozido ', porcao: '3 Ramos (69,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Couve-manteiga Cozida ', porcao: '1 Colher de Servir (42,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Ervilha em Conserva ', porcao: '1 Colher de Sopa (13,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Ervilha Fresca', porcao: '1 Colher de Sopa e Meia (19,50g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Escarola', porcao: '15 Folhas (83,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Espinafre Cozido', porcao: '3 Colheres de Sopa (60,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Jiló Cozido ', porcao: '1 Colher de Sopa e Meia (40,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Mostarda', porcao: '8 Folhas (83,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Palmito em Conserva ', porcao: '2 Unidades (100,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Pepino Japonês', porcao: '1 Unidade (130,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Pepino Picado ', porcao: '4 Colheres de Sopa (116,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Pimentão Cru Fatiado', porcao: '10 Fatias (70,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Pimentão Cru Picado ', porcao: '3 Colheres de Sopa (72,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Quiabo Cozido ', porcao: '2 Colheres de Sopa (80,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Rabanete', porcao: '3 Unidades (102,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Repolho Branco Cozido ', porcao: '5 Colheres de Sopa (75,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Repolho Branco Cru Picado ', porcao: '6 Colheres de Sopa (72,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Repolho Roxo Cru Picado ', porcao: '5 Colheres de Sopa (60,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Rúcula', porcao: '15 Folhas (83,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Salsão Cru', porcao: '2 Colheres de Sopa (38,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Tomate Caqui', porcao: '2 Fatias e Meia (75,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Tomate Cereja ', porcao: '7 Unidades (70,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Tomate Gaúcho ', porcao: '4 Fatias (80,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Vagem ', porcao: '2 Unidades (11,00g/ml)', grupo: legumesVerduras._id},
+                            {nome: 'Vagem Cozida', porcao: '2 Colheres de Sopa (44,00g/ml)', grupo: legumesVerduras._id},
+                            
+                            
+                            {nome: 'Ervilha Seca Cozida ', porcao: '2 Colheres de Sopa e Meia (73,00g/ml)', grupo: leguminosas._id},
+                            {nome: 'Feijão Branco Cozido', porcao: '1 Colher de Sopa e Meia (48,00g/ml)', grupo: leguminosas._id},
+                            {nome: 'Feijão Cozido (Somente Grãos) ', porcao: '2 Colheres de Sopa (50,00g/ml)', grupo: leguminosas._id},
+                            {nome: 'Feijão Preto Cozido (50% caldo) ', porcao: '1 Concha (86,00g/ml)', grupo: leguminosas._id},
+                            {nome: 'Grão de Bico Cozido ', porcao: '1 Colher de Sopa e Meia (36,00g/ml)', grupo: leguminosas._id},
+                            {nome: 'Lentilha Cozida ', porcao: '2 Colheres de Sopa (48,00g/ml)', grupo: leguminosas._id},
+                            {nome: 'Soja Cozida ', porcao: '1 Colher de Servir (43,00g/ml)', grupo: leguminosas._id},
+                            
+                            {nome: 'Coalhada', porcao: 'Meio Copo de Reiqueijão (100,00g/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Iogurte Desnatado de Frutas ', porcao: '1 Pote(140,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Iogurte Integral Natural', porcao: '1 Copo de Requeijão (200,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Iorgurte Desnatado Natural', porcao: '1 Copo(200,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Leite de Cabra Integral ', porcao: '1 Copo(180,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Leite em Pó Desnatado ', porcao: '2 Colheres de Sopa(30,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Leite em Pó Integral', porcao: '2 Colheres de Sopa(30,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Leite em Pó Integral tipo "Ninho" ', porcao: '2 Colheres de Sopa(30,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Leite Integral Longa Vida ', porcao: '1 Copo(180,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Leite Semi-desnatado Longa Vida ', porcao: '1 Copo(180,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Leite Tipo B 3,5% de Gordura', porcao: '1 Copo(180,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Leite Tipo C 3,0% de Gordura', porcao: '1 Copo(180,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Queijo Pasteurizado ', porcao: '2 Unidades(35,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Queijo Prato', porcao: '2 Fatias(40,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Queijo Provolone', porcao: '1 Fatia (35,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Queijo Tipo Minas ', porcao: '1 Fatia e Meia(50,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Queijo Tipo Mussarela ', porcao: '3 Fatias(45,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Queijo Tipo Parmesão Ralado ', porcao: '2 Colheres de Sopa(30,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Requeijão Cremoso ', porcao: '1 Colher de Sopa e Meia (45,00/ml)', grupo: leiteQueijoIogurte._id},
+                            {nome: 'Ricota', porcao: '2 Fatias(100,00/ml)', grupo: leiteQueijoIogurte._id},
+                            
+                            
+                            {nome: 'Azeite de Dendê ', porcao: '1/2 Colher de Sopa (9,20g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Azeite de Oliva ', porcao: '1 Colher de Sopa (7,60g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Bacon (gordura) ', porcao: '1/2 Fatia (7,50g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Banha de Porco', porcao: '1/2 Colher de Sopa (7,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Creme Vegetal ', porcao: '1 Colher de Sopa (14,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Halvarina ', porcao: '1 Colher de Sopa (20,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Manteiga', porcao: '1/2 Colher de Sopa (10,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Margarina Culinária ', porcao: '1/10 de Tablete (10,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Margarina Líquida ', porcao: '1 Colher de Sopa (10,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Margarina Vegetal ', porcao: '1/2 Colher de Sopa (9,70g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Óleo Vegetal de Canola', porcao: '1 Colher de Sopa (8,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Óleo Vegetal de Girassol', porcao: '1 Colher de Sopa (8,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Óleo Vegetal de Milho ', porcao: '1 Colher de Sopa (8,00g/ml)', grupo: oleosGorduras._id},
+                            {nome: 'Óleo Vegetal de Soja', porcao: '1 Colher de Sopa (8,00g/ml)', grupo: oleosGorduras._id},
+                            ];
 
         carboidratos.save(function (err) {
             if (err) return handleError(err);
@@ -122,6 +221,22 @@ module.exports = function(app){
         });
 
         frutas.save(function (err) {
+            if (err) return handleError(err);
+        });
+        
+        legumesVerduras.save(function (err) {
+            if (err) return handleError(err);
+        });
+        
+        leguminosas.save(function (err) {
+            if (err) return handleError(err);
+        });
+        
+        leiteQueijoIogurte.save(function (err) {
+            if (err) return handleError(err);
+        });
+        
+        oleosGorduras.save(function (err) {
             if (err) return handleError(err);
         });
 
