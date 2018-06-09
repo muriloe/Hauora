2db15a1fcd9ae396e8d7f61c997a2e079a4a53a5
Murilo Erhardt <murilo0121@gmail.com>
2018-06-08 21:22:48 -0300

    scroll infinito from hell

--- front-end/src/app/pages/consulta/consulta-antropometria/historico-consulta-card/historico-consulta-card.component.ts
+++ front-end/src/app/pages/consulta/consulta-antropometria/historico-consulta-card/historico-consulta-card.component.ts
@@ -4,6 +4,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConsultaService } from '../../consulta.service';
 import { Router } from '@angular/router';
+// tslint:disable-next-line:max-line-length
 import { VisualizarCardapioModalComponent } from '../../../perfil/visualizar-consulta-modal/visualizar-consulta-modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -35,14 +36,14 @@ export class HistoricoConsultaCardComponent implements OnInit {
               },
           );
     }
-    
+
     verConsulta(consulta) {
         // tslint:disable-next-line:max-line-length
         const activeModal = this.modalService.open(VisualizarCardapioModalComponent, { size: 'lg', container: 'nb-layout' });
         activeModal.componentInstance.modalHeader = 'Large Modal';
         activeModal.componentInstance.consulta = consulta;
     }
-    
+
     verCliente(clienteId) {
       const urlRedirect = 'pages/perfil?cliente=' + clienteId;
       this.router.navigate(['pages/perfil'], { queryParams: { cliente: clienteId } });
--- front-end/src/app/pages/notificacoes/notificacao.component.ts
+++ front-end/src/app/pages/notificacoes/notificacao.component.ts
@@ -4,6 +4,7 @@ import { NotificacaoService } from './notificacao.service';
 import { NbMenuItem } from '@nebular/theme';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
+// tslint:disable-next-line:max-line-length
 import { VisualizarCardapioModalComponent } from '../perfil/visualizar-consulta-modal/visualizar-consulta-modal.component';
 import { PostagemModalComponent } from '../postagens/postagem-card/postagem-modal/postagem-modal.component';
 
@@ -15,51 +16,74 @@ import { PostagemModalComponent } from '../postagens/postagem-card/postagem-moda
     styleUrls: ['./notificacao.component.scss'],
   })
 
-export class NotificacaoComponent implements OnInit{
+export class NotificacaoComponent implements OnInit {
     notificacoes: Notificacao[] = [];
     indice = 0;
-    
+    ativarBotaoCarregarMais: boolean = true;
+
+
+    // tslint:disable-next-line:max-line-length
     constructor(private router: Router, private modalService: NgbModal, private notificacaoService: NotificacaoService) {}
-    
+
      ngOnInit() {
          this.getListaNotificacoes();
      }
-     
+
      getListaNotificacoes() {
         this.notificacaoService.buscarConsultas(this.indice)
         .subscribe(
             (notificacoes: Notificacao[]) => {
                 this.notificacoes = notificacoes;
+                if (notificacoes.length !== 10) {
+                    this.ativarBotaoCarregarMais = false;
+                } else {
+                    this.indice = this.indice + 10;
+                }
             },
         );
     }
-    
-    
+
+    carregarMais() {
+        this.notificacaoService.buscarConsultas(this.indice)
+        .subscribe(
+            (notificacoes: Notificacao[]) => {
+
+                for (const notificacao of notificacoes) {
+                    this.notificacoes.push(notificacao);
+                }
+                if (notificacoes.length !== 11) {
+                    this.ativarBotaoCarregarMais = false;
+                } else {
+                    this.indice = this.indice + 10;
+                }
+            },
+        );
+    }
+
     verDetalhe(notificacao) {
-        if(!notificacao.consulta[0] && !notificacao.postagem[0]) {
-            console.log('notifacaca de trpca de cardapio');
+        if (!notificacao.consulta[0] && !notificacao.postagem[0]) {
             const urlRedirect = 'pages/perfil?cliente=' + notificacao.cliente;
             this.router.navigate(['pages/perfil'], { queryParams: { cliente: notificacao.cliente } });
         }
-        if(notificacao.consulta[0]) {
-            console.log('notifacaca consulta');
+        if (notificacao.consulta[0]) {
+            // tslint:disable-next-line:max-line-length
             const activeModal = this.modalService.open(VisualizarCardapioModalComponent, { size: 'lg', container: 'nb-layout' });
             activeModal.componentInstance.modalHeader = 'Large Modal';
             activeModal.componentInstance.consulta = notificacao.consulta[0];
         }
-        if(notificacao.postagem[0]) {
-            
+        if (notificacao.postagem[0]) {
+
              this.notificacaoService.obterPostagem(notificacao.postagem[0]._id)
             .subscribe(
                 (notificacoes: Notificacao) => {
-                    var not = notificacoes;
+                    const notific = notificacoes;
+                    // tslint:disable-next-line:max-line-length
                     const activeModal = this.modalService.open(PostagemModalComponent, { size: 'lg', container: 'nb-layout' });
                     activeModal.componentInstance.modalHeader = 'Large Modal';
-                    activeModal.componentInstance.postagem = not;
+                    activeModal.componentInstance.postagem = notific;
                 },
             );
 
         }
     }
-     
 }
