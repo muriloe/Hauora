6843183e15732b9d38344be8a8b22f64f638780a
Murilo Erhardt <murilo0121@gmail.com>
2018-06-10 14:10:18 -0300

    Atualizando preconsulta

--- back-end/business/anamnese/anamneseBusiness.js
+++ back-end/business/anamnese/anamneseBusiness.js
@@ -14,6 +14,7 @@ exports.criarAnamneseCompleta = function(dat){
             Anamnese.find({cliente: dat.cliente._id}, function(err, anamnese) {
                 if (!err) {
                     console.log("entrou onde queria");
+                    console.log(anamnese.doenca);
                     listaIdDoenca = [];
                     listaIdDoenca = anamnese.doenca;
                     listaIdConsumo = [];
